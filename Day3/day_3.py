96e4809cb6203e88f296525ef7e5d97dafabc728
DCarist <53244900+DCarist@users.noreply.github.com>
2023-12-05 18:03:06 -0500

    adding _compute_number and _check_right_left

--- Day3/day_3.py
+++ Day3/day_3.py
@@ -18,6 +18,7 @@ class EngineComponent():
     def __str__(self):
         return f'{self.value}:({self.x_pos},{self.y_pos}) {self.is_symbol}'
 
+    
 class EngineSchematic():
     """
 
@@ -27,8 +28,9 @@ class EngineSchematic():
         #Assume we've taken the full string and split it into Y already (list)
         self.schematic:list[list[str]] =[[]]
         #schematic [x][y][value]
-        self.component_list: list[EngineComponent] = []
+        self.component_list: list[EngineComponent] = [[]]
         y=0
+        self.symbol_list: list[EngineComponent] = []
         for line in input_list:
             x =0
             for char in line:
@@ -36,14 +38,70 @@ class EngineSchematic():
                 #makes a simple nested list.
                 is_symbol = char in SYMBOL_LIST
                 cur_char = EngineComponent(char,x,y,is_symbol)
-                self.component_list.append(cur_char)
+                self.component_list[y].append(cur_char)
+                if is_symbol:
+                    self.symbol_list.append(cur_char)
                 x +=1 
 
             y +=1
             self.schematic.append([])
+            self.component_list.append([])
+        
+        #print(self.component_list)
+        #print(self.symbol_list)
+        self.find_symbol_code()
+
+    def find_symbol_code(self):
+        
+        for symbol in self.symbol_list:
+            self._compute_number(symbol.x_pos,symbol.y_pos)
+            #don't want out of index or other funny business
+
+        #our components are in a list of lists [y][x]
+
+
+    def _compute_number(self, x, y):
+        """
+            <- X-1,Y-1 X,Y-1, X+1, Y-1 ->
+            <- X-1, Y  X,Y  , X+1, Y   ->
+            <- X-1,Y+1 X,Y+1, X+1, Y+1 ->
+        """
+        
+        #check up, down
+        self._check_right(x,y-1)
+        self._check_left(x,y+1)
+        self._check_right(x,y+1)
+
+    def _check_right(self, x,y):
+        #start in the middle
+        number_string:str = ''
+        cur_pos = self.component_list[y][x].value
+        while cur_pos != BLANK:
+
+            number_string += cur_pos
+            x+= 1
+            cur_pos = self.component_list[y][x].value
+        
+        if number_string != '':
+            return int(number_string)
+        else:
+            return
+        
+    def _check_left(self,x,y):
+        number_string:str = ''
+        cur_pos = self.component_list[y][x].value
+        while cur_pos != BLANK:
+            number_string = cur_pos + number_string
+            x-= 1
+            cur_pos = self.component_list[y][x].value
+        
+        print(number_string)
+        if number_string != '':
+            return int(number_string)
+        else:
+            return
+        
     
-        print(self.schematic[1][3])
-        print(self.component_list)
 
 def day_3_part_1(input:str):
     
--- tests/test_day3.py
+++ tests/test_day3.py
@@ -1,2 +1,17 @@
 from Day3 import day_3
 
+TEST_STRING = """467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598.."""
+
+def test_day_2():
+    #string_list = TEST_STRING.split('\n')
+    day_3.day_3_part_1(TEST_STRING)
+
