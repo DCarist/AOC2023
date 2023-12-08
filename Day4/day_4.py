29460b21d0e8856c8da2eca66b61cdbcbf99a467
Douglas Caristo <Tehstudent@gmail.com>
2023-12-08 14:27:41 -0500

    day 4 part 2 complete.

--- Day4/day_4.py
+++ Day4/day_4.py
@@ -31,7 +31,7 @@ class Card():
         return len(self.winning_numbers.intersection(self.card_numbers))
     
     def __repr__(self):
-        return f'{self.card_number}: {self.duplicates_generated()}'
+        return f' C: {self.card_number} WN:{self.duplicates_generated()}'
     
 class Deck():
 
@@ -39,34 +39,43 @@ class Deck():
     card_count:int
     current_card:int=0
 
-    def __init__(self,deck:list[Card]=[[]]):
+    def __init__(self,deck:list[list[Card]]=[[]]):
         self.deck = deck
         self.card_count = len(self.deck)
 
     def append(self, card:Card) -> None:
         self.deck[self.card_count-1].append(card)
+        self.deck.append([])
         self.card_count = len(self.deck)
 
     def copy_card(self,card:Card, position:int) -> None:
-        self.deck.insert(position, card)
+        self.deck[position].insert(position, card)
         self.card_count = len(self.deck)
 
     def process_cards(self):
-        
+        self.deck.pop(-1)
         for card_list in self.deck:
             self.cards_to_copy(card_list)
-            cards_to_copy = card.duplicates_generated()
-            card_offset = 1
-            while cards_to_copy >0:
-                
-                cards_to_copy -= 1
+
+    def cards_to_copy(self, card_list:list[Card]):
+        print(f'Starting Line: {card_list[0]}')
+        for card in card_list:
+            cards_copy = card.duplicates_generated()
+            card_offset = self.current_card +1
+            while cards_copy >0:
+                self.copy_card(self.deck[card_offset][0], card_offset)
+                cards_copy -= 1
                 card_offset += 1
                 
-            self.current_card += 1
+        self.current_card += 1
 
             #okay lol it works but I don't know why.
     def __len__(self):
-        return len(self.deck)
+        length = 0
+        for card_list in self.deck:
+            length += len(card_list)
+        
+        return length
     
     def return_location_of_next_card(self, cur_card:Card):
         index = cur_card.card_number   
@@ -88,7 +97,7 @@ def day_4_part_2(string:str) -> int:
 
     part_2_deck.process_cards()
 
-    return part_2_deck.current_card
+    return len(part_2_deck)
             
 
 def main():
